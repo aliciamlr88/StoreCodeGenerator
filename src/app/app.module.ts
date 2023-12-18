86d2f7da52872cd42c6951d95672f46d08e86937
aliciamlr88 <aliciamlr88@gmail.com>
2023-12-18 09:55:21 -0400

    Adding the code generator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { CodeGeneratorComponent } from './code-generator/code-generator.component'; 
+import { CodeGeneratorComponent } from './code-generator/code-generator.component';
+import { BarCodeComponent } from './bar-code/bar-code.component'; 
+import { BarcodeGeneratorAllModule } from '@syncfusion/ej2-angular-barcode-generator';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CodeGeneratorComponent
+    CodeGeneratorComponent,
+    BarCodeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BarcodeGeneratorAllModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/bar-code/bar-code.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bar-code',
+  templateUrl: './bar-code.component.html',
+  styleUrls: ['./bar-code.component.css']
+})
+export class BarCodeComponent {
+
+}
