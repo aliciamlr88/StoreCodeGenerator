132306e3f82625e9dcb2b9932704a7475406a1ef
aliciamlr88 <aliciamlr88@gmail.com>
2023-12-20 11:55:18 -0400

    Adding some colors and sizes

--- src/app/data/color.ts
+++ src/app/data/color.ts
@@ -24,7 +24,90 @@ export let COLORS: Color[] = [
         code: '100',
         name: 'BLACK',
         htmlCode: '#000000'
-    }
-   
+    },
+    {
+        id: '5',
+        code: '707',
+        name: 'DK SMOKE',
+        htmlCode: '#2e2926'
+    },
+    {
+        id: '6',
+        code: '488',
+        name: 'MED INDIGO',
+        htmlCode: '#2e2926'
+    },
+    {
+        id: '7',
+        code: '582',
+        name: 'DK MARIGOLD',
+        htmlCode: '#2e2926'
+    },
+    {
+        id: '8',
+        code: '732',
+        name: 'SAGE',
+        htmlCode: '#2e2926'
+    },
+    {
+        id: '9',
+        code: '283',
+        name: 'SPRIG',
+        htmlCode: '#2e2926'
+    },
+    {
+        id: '10',
+        code: '360',
+        name: 'PACIFIC BLUE',
+        htmlCode: '#2e2926'
+    },
+    {
+        id: '11',
+        code: '512',
+        name: 'DEEP EGGPLANT',
+        htmlCode: '#2e2926'
+    },
+    {
+        id: '12',
+        code: '854',
+        name: 'ANTIQUE BRONZE',
+        htmlCode: '#2e2926'
+    },
+    {
+        id: '13',
+        code: '082',
+        name: 'DK MINT',
+        htmlCode: '#2e2926'
+    },
+    {
+        id: '14',
+        code: '685',
+        name: 'DK SMOKE HTR',
+        htmlCode: '#2e2926'
+    },
+    {
+        id: '15',
+        code: '712',
+        name: 'DK EVERGREEN',
+        htmlCode: '#2e2926'
+    },
+    {
+        id: '16',
+        code: '054',
+        name: 'CHARTREUSE',
+        htmlCode: '#2e2926'
+    },
+    {
+        id: '17',
+        code: '956',
+        name: 'DUSTY RED',
+        htmlCode: '#2e2926'
+    },
+    {
+        id: '18',
+        code: '268',
+        name: 'AIRFORCE BLUE',
+        htmlCode: '#2e2926'
+    },
    
 ];
--- src/app/data/size.ts
+++ src/app/data/size.ts
@@ -35,6 +35,56 @@ export let SIZES: Size[] = [
         id: '7',
         code: '0080',
         name: 'XXLARGE'
-    }
+    },
+    {
+        id: '8',
+        code: '1000',
+        name: 'SIZE 0'
+    },
+    {
+        id: '9',
+        code: '1002',
+        name: 'SIZE 2'
+    },
+    {
+        id: '10',
+        code: '1004',
+        name: 'SIZE 4'
+    },
+    {
+        id: '11',
+        code: '1006',
+        name: 'SIZE 6'
+    },
+    {
+        id: '12',
+        code: '1008',
+        name: 'SIZE 8'
+    },
+    {
+        id: '13',
+        code: '1010',
+        name: 'SIZE 10'
+    },
+    {
+        id: '14',
+        code: '1012',
+        name: 'SIZE 12'
+    },
+    {
+        id: '15',
+        code: '1014',
+        name: 'SIZE 14'
+    },
+    {
+        id: '16',
+        code: '1016',
+        name: 'SIZE 16'
+    },
+    {
+        id: '17',
+        code: '1018',
+        name: 'SIZE 18'
+    },
    
 ];
