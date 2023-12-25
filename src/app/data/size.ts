4fd22b21e3cb39389e857f3b9dc12dc8d08dc36f
aliciamlr88 <aliciamlr88@gmail.com>
2023-12-25 15:19:45 -0400

    Adding the sizes

--- src/app/data/size.ts
+++ src/app/data/size.ts
@@ -1,3 +1,239 @@
 import { Size } from "../models/size";
 
-export let SIZES: any = [{"id":1,"code":"0000","name":"ONESIZE"}];
+export let SIZES: any = [
+    {
+        "id": 1,
+        "code": "0000",
+        "name": "ONESIZE"
+    },
+    {
+        "id": 2,
+        "code": "0030",
+        "name": "XSMALL"
+    },
+    {
+        "id": 3,
+        "code": "0040",
+        "name": "SMALL"
+    },
+    {
+        "id": 4,
+        "code": "0050",
+        "name": "MEDIUM"
+    },
+    {
+        "id": 5,
+        "code": "0060",
+        "name": "LARGE"
+    },
+    {
+        "id": 6,
+        "code": "0070",
+        "name": "XLARGE"
+    },
+    {
+        "id": 7,
+        "code": "0080",
+        "name": "XXLARGE"
+    },
+    {
+        "id": 8,
+        "code": "1000",
+        "name": "SIZE 0"
+    },
+    {
+        "id": 9,
+        "code": "1002",
+        "name": "SIZE 2"
+    },
+    {
+        "id": 10,
+        "code": "1004",
+        "name": "SIZE 4"
+    },
+    {
+        "id": 11,
+        "code": "1006",
+        "name": "SIZE 6"
+    },
+    {
+        "id": 12,
+        "code": "1008",
+        "name": "SIZE 8"
+    },
+    {
+        "id": 13,
+        "code": "1010",
+        "name": "SIZE 10"
+    },
+    {
+        "id": 14,
+        "code": "1012",
+        "name": "SIZE 12"
+    },
+    {
+        "id": 15,
+        "code": "1014",
+        "name": "SIZE 14"
+    },
+    {
+        "id": 16,
+        "code": "1016",
+        "name": "SIZE 16"
+    },
+    {
+        "id": 17,
+        "code": "1018",
+        "name": "SIZE 18"
+    },
+    {
+        "id": 18,
+        "code": "1030",
+        "name": "SIZE 30"
+    },
+    {
+        "id": 19,
+        "code": "1032",
+        "name": "SIZE 32"
+    },
+    {
+        "id": 20,
+        "code": "1033",
+        "name": "SIZE 33"
+    },
+    {
+        "id": 21,
+        "code": "1034",
+        "name": "SIZE 34"
+    },
+    {
+        "id": 22,
+        "code": "1035",
+        "name": "SIZE 35"
+    },
+    {
+        "id": 23,
+        "code": "1036",
+        "name": "SIZE 36"
+    },
+    {
+        "id": 24,
+        "code": "1038",
+        "name": "SIZE 38"
+    },
+    {
+        "id": 25,
+        "code": "1040",
+        "name": "SIZE 40"
+    },
+    {
+        "id": 26,
+        "code": "9154",
+        "name": "30X30"
+    },
+    {
+        "id": 27,
+        "code": "3162",
+        "name": "30X32"
+    },
+    {
+        "id": 28,
+        "code": "9278",
+        "name": "32X30"
+    },
+    {
+        "id": 29,
+        "code": "9286",
+        "name": "32X32"
+    },
+    {
+        "id": 30,
+        "code": "9294",
+        "name": "32X34"
+    },
+    {
+        "id": 31,
+        "code": "9340",
+        "name": "33X30"
+    },
+    {
+        "id": 32,
+        "code": "9348",
+        "name": "33X32"
+    },
+    {
+        "id": 33,
+        "code": "9356",
+        "name": "33X34"
+    },
+    {
+        "id": 34,
+        "code": "9402",
+        "name": "34X30"
+    },
+    {
+        "id": 35,
+        "code": "9410",
+        "name": "34X32"
+    },
+    {
+        "id": 36,
+        "code": "9418",
+        "name": "34X34"
+    },
+    {
+        "id": 37,
+        "code": "9464",
+        "name": "35X30"
+    },
+    {
+        "id": 38,
+        "code": "9472",
+        "name": "35X32"
+    },
+    {
+        "id": 39,
+        "code": "9480",
+        "name": "35X34"
+    },
+    {
+        "id": 40,
+        "code": "9526",
+        "name": "36X30"
+    },
+    {
+        "id": 41,
+        "code": "9534",
+        "name": "36X32"
+    },
+    {
+        "id": 42,
+        "code": "9542",
+        "name": "36X34"
+    },
+    {
+        "id": 43,
+        "code": "9588",
+        "name": "38X30"
+    },
+    {
+        "id": 44,
+        "code": "9596",
+        "name": "38X32"
+    },
+    {
+        "id": 45,
+        "code": "9604",
+        "name": "38X34"
+    },
+    {
+        "id": 46,
+        "code": "9650",
+        "name": "40X30"
+    },
+    {
+        "id": 47,
+        "code": "9658",
+        "name": "40X32"
+    }
+];
